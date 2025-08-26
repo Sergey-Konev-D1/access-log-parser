277f0d99b3971217a078f656d7873159cd1ba67b
Sergey Konev <skon88@mail.ru>
2025-08-26 22:35:20 +0300

    Задание по теме ""Синтаксис языка" и “Базовые типы данных”" + поправил частное, тк считалось некорректно + привел к одному стилю код вследствии измененеия частного

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Введите первое число: ");
         int numb1 = new Scanner(System.in).nextInt();
+
         System.out.println("Введите второе число: ");
         int numb2 = new Scanner(System.in).nextInt();
 
-        System.out.println("Сумма: " + (numb1+numb2));
-        System.out.println("Разность: " + (numb1-numb2));
-        System.out.println("Произведение: " + (numb1*numb2));
-        System.out.println("Частное: " + (double)(numb1/numb2));
+        int sum = numb1+numb2;
+        System.out.println("Сумма: " + sum);
 
+        int dif = numb1-numb2;
+        System.out.println("Разность: " + dif);
+
+        int prod =  numb1*numb2;
+        System.out.println("Произведение: " + prod);
+
+        double quot = (double)numb1/numb2;
+        System.out.println("Частное: " + quot);
 
     }
 }
