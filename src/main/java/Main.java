e85f47a735bfc749dcc93db229177c68ffe47658
Sergey Konev <skon88@mail.ru>
2025-09-07 23:43:58 +0300

    выполнен запрос пути к файлу, добавлены проверки на существование файла и на проверку директории, добавлен счетчик корректно указаных файлов

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,30 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите Enter");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int counter = 0;
 
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            String path = new Scanner(System.in).nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if(!fileExists || isDirectory){
+                if(!fileExists){
+                    System.out.println("Указанный файл не существует");
+                }
+                else{
+                    System.out.println("Указанный путь является путём к папке");
+                }
+                continue;
+            }
+            System.out.println("Путь указан верно");
+            counter++;
+            System.out.println("Это файл номер " + counter);
+        }
     }
 }
